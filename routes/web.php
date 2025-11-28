b52fd72e9a5fa55579b965cd194910f83d3010a6
Mariam-AbdElKader <mariamabdelkader62@gmail.com>
2025-11-28 23:50:25 +0200

    redirect homepage to admin posts index

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('admin.posts.index');
 });
 
 Route::prefix('admin')->name('admin.')->group(function () {
