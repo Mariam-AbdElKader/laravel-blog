b6d76b09cd1f57c4a48b2178d8a7cf49f7f5f370
Mariam-AbdElKader <mariamabdelkader62@gmail.com>
2025-12-02 17:55:41 +0200

    remove: useless search bar

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -8,7 +8,7 @@
     <!-- Table Section -->
     <div class="bg-gray-800 p-6 rounded-lg shadow-lg">
         <div class="flex justify-between items-center mb-4">
-            <input type="text" placeholder="Search..." class="w-1/3 px-4 py-2 bg-gray-700 rounded-md" />
+            {{-- <input type="text" placeholder="Search..." class="w-1/3 px-4 py-2 bg-gray-700 rounded-md" /> --}}
             {{-- <select class="px-4 py-2 bg-gray-700 rounded-md">
                 <option>10</option>
                 <option>20</option>
